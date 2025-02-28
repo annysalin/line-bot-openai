966442ba3ae14197dcfb102878e434a71819a44c
annysalin <annysa@huayuworld.org>
2025-02-28 18:57:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from linebot.v3.messaging import MessagingApi, Configuration, ApiClient
-from linebot.v3.webhook import WebhookHandler
+from linebot.v3.webhooks import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging.models import TextMessage
-from linebot.v3.webhook.models import MessageEvent, TextMessageContent
+from linebot.v3.webhooks.models import MessageEvent, TextMessageContent
 import openai
 import os
 
